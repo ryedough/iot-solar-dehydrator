cffb90a4a880390ea9940f54cfdd939c01c81c46
ryedough <adiwaskita@proton.me>
2026-07-12 19:30:20 +0700

    feat(esp8266): added spi slave for esp

--- esp8266/main/main.c
+++ esp8266/main/main.c
@@ -17,32 +17,6 @@ static const char *TAG = "spi_slave_example";
 #define SPI_WRITE_BUFFER_MAX_SIZE               2048
 #define SPI_READ_BUFFER_MAX_SIZE                1024
 
-//#define SPI_SLAVE_TEST_SEND    // Define the macro is slave send mode, delete will be master send mode
-
-#ifdef SPI_SLAVE_TEST_SEND
-static void IRAM_ATTR spi_slave_write_master_task(void *arg)
-{
-    static uint8_t write_data[SPI_WRITE_BUFFER_MAX_SIZE];
-    uint32_t total_tx_count = 0;
-
-    for (int32_t loop = 0; loop < SPI_WRITE_BUFFER_MAX_SIZE; loop++) {
-        write_data[loop] = 0xa3;
-    }
-
-    vTaskDelay(5000 / portTICK_RATE_MS);
-    ESP_LOGI(TAG, "SPI slave start send data");
-    for (;;) {
-        total_tx_count += hspi_slave_logic_write_data(write_data, SPI_READ_BUFFER_MAX_SIZE, portMAX_DELAY);
-
-        if (total_tx_count >= 10*1024*1024) {
-            ESP_LOGI(TAG, "send done; %d bytes\r\n", total_tx_count);
-            break;
-        }
-    }
-    vTaskDelete(NULL);
-}
-#else
-
 static void IRAM_ATTR spi_slave_read_master_task(void *arg)
 {
     static uint8_t read_data[SPI_READ_BUFFER_MAX_SIZE];
@@ -55,7 +29,6 @@ static void IRAM_ATTR spi_slave_read_master_task(void *arg)
         memset(read_data, 0x0, SPI_READ_BUFFER_MAX_SIZE);
     }
 }
-#endif
 
 void app_main(void)
 {
@@ -74,13 +47,8 @@ void app_main(void)
     spi_init(HSPI_HOST, &spi_config);
 
     hspi_slave_logic_device_create(SPI_SLAVE_HANDSHAKE_GPIO, 1, SPI_WRITE_BUFFER_MAX_SIZE, SPI_READ_BUFFER_MAX_SIZE);
-#ifdef SPI_SLAVE_TEST_SEND
-    xTaskCreate(spi_slave_write_master_task, "spi_slave_write_master_task", 2048, NULL, 2, NULL);
-#else
-    // create spi_slave_read_master_task
+
     xTaskCreate(spi_slave_read_master_task, "spi_slave_read_master_task", 2048, NULL, 5, NULL);
-    xTaskCreate(spi_slave_receive_count_task, "spi_slave_receive_count_task", 2048, NULL, 4, NULL);
-#endif
 }
 
 
